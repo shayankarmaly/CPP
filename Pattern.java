2c26c963f0209da66447a72808df20fafaf2697c
shayankarmaly <79032128+shayankarmaly@users.noreply.github.com>
2021-04-09 01:16:48 -0700

    Add files via upload

---
+++ Pattern.java
@@ -0,0 +1,201 @@
+import java.util.Scanner;
+
+public class Diamond{
+
+public static void main(String [] args){
+
+  
+
+int size =-1;
+
+String choice;
+
+Scanner scnr=new Scanner(System.in);
+
+while(size ==-1){
+
+System.out.print("enter diamod size"+ "(" + "\"small\"" +"\",average\"" + "\",tall\"" +") : ");
+
+choice = scnr.nextLine();
+
+size =checkSize(choice);
+
+}
+
+System.out.print("enter pattern character: ");
+
+char ch = scnr.next().charAt(0);
+
+Pattern.displayDiamond(size,ch);
+
+Pattern.displayHollowDiamond(size,ch);
+
+}
+
+public static int checkSize(String size){
+
+if(size.equals("short")){
+
+return 6;
+
+}
+
+if(size.equals("average")){
+
+return 12;
+
+}
+
+if(size.equals("tall")){
+
+return 24;
+
+}
+
+return -1;
+
+}
+
+public static void displayHollowDiamond(int size,char ch){
+
+int i, j, space, k = 0;
+
+  
+
+System.out.println();
+
+System.out.println();
+
+System.out.println();
+
+for (i = 1; i <= size; i++) {
+
+for (j = 1; j <= size - i; j++) {
+
+System.out.print(" ");
+
+}
+
+while (k != (2 * i - 1)) {
+
+if (k == 0 || k == 2 * i - 2)
+
+System.out.print(ch);
+
+else
+
+System.out.print(" ");
+
+k++;
+
+}
+
+k = 0;
+
+System.out.println();
+
+}
+
+size--;
+
+for (i = size; i >= 1; i--) {
+
+for (j = 0; j <= size - i; j++) {
+
+System.out.print(" ");
+
+}
+
+k = 0;
+
+while (k != (2 * i - 1)) {
+
+if (k == 0 || k == 2 * i - 2)
+
+System.out.print(ch);
+
+else
+
+System.out.print(" ");
+
+k++;
+
+}
+
+System.out.println();
+
+}
+
+}
+
+public static void displayDiamond(int size,char ch){
+
+int i=1;
+
+int j;
+
+while(i<=size)
+
+{
+
+j=1;
+
+while(j++<=size-i)
+
+{
+
+System.out.print(" ");
+
+}
+
+j=1;
+
+while(j++<=i*2-1)
+
+{
+
+System.out.print(ch);
+
+}
+
+System.out.println();
+
+i++;
+
+}
+
+i=size-1;
+
+while(i>0)
+
+{
+
+j=1;
+
+while(j++<=size-i)
+
+{
+
+System.out.print(" ");
+
+}   
+
+j=1;
+
+while(j++<=i*2-1)
+
+{
+
+System.out.print(ch);
+
+}
+
+System.out.println();
+
+i--;
+
+}
+
+}
+
+}
